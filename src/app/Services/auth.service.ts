5ea24d22092c30bbb99e40cb5c551ba9ed3aff9c
Rambabu Kumar <54993494+Ramkumar908@users.noreply.github.com>
2023-05-17 08:04:17 +0530

    Update auth.service.ts
    Add some new code in auth service.ts

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +9,27 @@ export class AuthService {
 
   constructor() { }
 
-  isLoggedIn()
-  {
-    return !!localStorage.getItem("token");
+  getToken(): string {
+    return localStorage.getItem("token");
   }
+
+  getTokenExpirationDate(token: string): Date {
+    const decoded = jwt_decode(token);
+
+    if (decoded.exp === undefined) return null;
+
+    const date = new Date(0); 
+    date.setUTCSeconds(decoded.exp);
+    return date;
+  }
+
+  isTokenExpired(token?: string): boolean {
+    if(!token) token = this.getToken();
+    if(!token) return true;
+
+    const date = this.getTokenExpirationDate(token);
+    if(date === undefined) return false;
+    return !(date.valueOf() > new Date().valueOf());
+  }
+
 }
